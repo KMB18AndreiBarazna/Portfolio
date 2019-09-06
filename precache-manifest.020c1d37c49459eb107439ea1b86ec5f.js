3b1dc023f331339aae18a08fe076e259d52deb82
Lenovo <2andrewbor@gmail.com>
2019-09-06 21:33:36 +0300

    Updates

---
+++ precache-manifest.020c1d37c49459eb107439ea1b86ec5f.js
@@ -0,0 +1,38 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "05a01d8d452ab2ec09c264c727828dbf",
+    "url": "/Portfolio/index.html"
+  },
+  {
+    "revision": "dd1389bf9a1114030f51",
+    "url": "/Portfolio/static/css/main.5cc92f2b.chunk.css"
+  },
+  {
+    "revision": "5f82c6a4d11b4637181e",
+    "url": "/Portfolio/static/js/2.cdd25738.chunk.js"
+  },
+  {
+    "revision": "dd1389bf9a1114030f51",
+    "url": "/Portfolio/static/js/main.557300da.chunk.js"
+  },
+  {
+    "revision": "05a6bd0e60d5b4972fd5",
+    "url": "/Portfolio/static/js/runtime~main.02afc9dc.js"
+  },
+  {
+    "revision": "9b001e950733c03a160bde299db194c7",
+    "url": "/Portfolio/static/media/HTML.9b001e95.png"
+  },
+  {
+    "revision": "3fe9633b50c9647560e0042352d5e177",
+    "url": "/Portfolio/static/media/MyPhoto.3fe9633b.jpg"
+  },
+  {
+    "revision": "91dbbe1c79adcdcdd398abc21bd13035",
+    "url": "/Portfolio/static/media/Todolist.91dbbe1c.png"
+  },
+  {
+    "revision": "fa21136a71b3811e2bb04420a8738cfe",
+    "url": "/Portfolio/static/media/net2.fa21136a.png"
+  }
+]);
